c385e9985a67ad097cb2c9c8d2ccae4e420fdf35
Lisenok30 <lidia.utesheva@gmail.com>
2023-07-18 17:34:49 -0700

    Second commit

--- cypress/e2e/automation-text-store/add-multiple-items-to-basket.js
+++ cypress/e2e/automation-text-store/add-multiple-items-to-basket.js
@@ -2,23 +2,23 @@ import AutoStore_Homepage_PO from "../../support/pageObjects/automation-test-sto
 import AutoStore_Haircare_PO from "../../support/pageObjects/automation-test-store/AutoStore_Haircare_PO";
 /// <reference types = "Cypress"/>
 
-describe("Add multiple items to basket", () => {
-    const autoStore_Homepage_PO = new AutoStore_Homepage_PO();
-    const autoStore_HairCare_PO = new AutoStore_Haircare_PO();
-  
-    before(function () {
-      cy.fixture("products").then(function (data) {
-        globalThis.data = data;
-      });
-    });
-  
-    beforeEach(function () {
-        cy.clearAllLocalStorage();
-        cy.clearCookies();
-      autoStore_Homepage_PO.accessHomepage();
-      autoStore_Homepage_PO.clickOn_HairCare_Link();
-    });
-    it("Add specific items to basket", () => {
-      autoStore_HairCare_PO.addHairCareProductsToBasket();
+describe("Add multiple items to the basket", () => {
+  const autoStore_Homepage_PO = new AutoStore_Homepage_PO();
+  const autoStore_HairCare_PO = new AutoStore_Haircare_PO();
+
+  before(function () {
+    cy.fixture("products").then(function (data) {
+      globalThis.data = data;
     });
   });
+
+  beforeEach(function () {
+    cy.clearAllLocalStorage();
+    cy.clearCookies();
+    autoStore_Homepage_PO.accessHomepage();
+    autoStore_Homepage_PO.clickOn_HairCare_Link();
+  });
+  it("Add specific items to the basket", () => {
+    autoStore_HairCare_PO.addHairCareProductsToBasket();
+  });
+});
